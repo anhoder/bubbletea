1ad9a297d8c5a55a4380115099ba642436ad3a3a
anhoder <anhoder@88.com>
2021-03-06 00:16:49 +0800

    update. export method

--- tea.go
+++ tea.go
@@ -721,3 +721,14 @@ func (p *Program) Printf(template string, args ...interface{}) {
 		messageBody: fmt.Sprintf(template, args...),
 	}
 }
+
+type flusher interface {
+	flush()
+}
+
+// Rerender use last render ui to refresh.
+func (p *Program) Rerender() {
+	if f, ok := p.renderer.(flusher); ok {
+		f.flush()
+	}
+}
